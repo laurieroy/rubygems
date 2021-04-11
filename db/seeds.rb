e33d97effd724aa1e84c32386566c5e56f064424
lauretteroy <laurieroy.dev@gmail.com>
2021-04-10 21:12:47 -0600

    Add User to course; Seed; Add associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
 30.times do
 	Course.create!([{
 		title: Faker::Educator.course_name,
-		description: Faker::TvShows::GameOfThrones.quote
+		description: Faker::TvShows::GameOfThrones.quote,
+		user_id: User.first.id
 	}])
 end
