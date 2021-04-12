f3c5494bb8e86948fab4ba8848681284f8fddee0
lauretteroy <laurieroy.dev@gmail.com>
2021-04-11 22:34:49 -0600

    Add more fields to course

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 30.times do
 	Course.create!([{
 		title: Faker::Educator.course_name,
 		description: Faker::TvShows::GameOfThrones.quote,
-		user_id: User.first.id
+		short_description: Faker::Quote::famous_last_words,
+		user_id: User.first.id,
+		language: Faker::ProgrammingLanguage.name,
+		level: 'Beginner',
+		price: Faker::Number.between(from: 1000, to: 20000)
 	}])
 end
